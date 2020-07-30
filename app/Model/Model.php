fedcb2a7520b88c36972b0b662e6381a4cdabe86
Radan Mlčůch <mlcuch@net-wings.eu>
2020-07-30 20:45:06 +0200

    Repositories registration in orm model

--- app/Model/Model.php
+++ app/Model/Model.php
@@ -2,6 +2,14 @@
 
 namespace App\Model;
 
+/**
+ * Model
+ *
+ * @property-read AnswerRepository
+ * @property-read EntryCodeRepository
+ * @property-read QuestionRepository
+ * @property-read ResultRepository
+ */
 class Model extends \Nextras\Orm\Model\Model
 {
 
