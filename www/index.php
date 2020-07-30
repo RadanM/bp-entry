e4f360ec8b9741b504a63c984b7a463f4b3d7ae3
Radan Mlčůch <mlcuch@net-wings.eu>
2020-07-30 16:28:45 +0200

    Nette composer update & API init

--- www/index.php
+++ www/index.php
@@ -2,9 +2,17 @@
 
 declare(strict_types=1);
 
+use Apitte\Core\Application\IApplication as ApiApplication;
+use App\Bootstrap;
+use Nette\Application\Application as UIApplication;
+
 require __DIR__ . '/../vendor/autoload.php';
 
-App\Bootstrap::boot()
-	->createContainer()
-	->getByType(Nette\Application\Application::class)
-	->run();
+$isApi = substr($_SERVER['REQUEST_URI'], 0, 4) === '/api';
+$container = Bootstrap::boot()->createContainer();
+
+if ($isApi) {
+    $container->getByType(ApiApplication::class)->run();
+} else {
+    $container->getByType(UIApplication::class)->run();
+}
