847db4763e6bc5321edf5484fdb587797964b250
Radan Mlčůch <mlcuch@net-wings.eu>
2020-07-30 13:15:51 +0200

    Docker init

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -13,7 +13,9 @@ class Bootstrap
 	{
 		$configurator = new Configurator;
 
-		//$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
+        if (getenv('NETTE_DEVEL') === '1') {
+            $configurator->setDebugMode(true);
+        }
 		$configurator->enableTracy(__DIR__ . '/../log');
 
 		$configurator->setTimeZone('Europe/Prague');
